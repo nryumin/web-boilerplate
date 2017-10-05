5712426a426bf95bb2b72c1126edc374f3be9ca4
nryumin <ryumin.nikita@gmail.com>
2017-10-05 16:57:38 +0300

    sh files

---
+++ run_dev.sh
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml build
+docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml up
---
+++ run_prod.sh
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml build
+docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml up
