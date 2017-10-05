cd98a89796c7916f83f9714eb23244bcbdf24d72
nryumin <ryumin.nikita@gmail.com>
2017-10-05 14:43:31 +0300

    some staff2

--- web/app.py
+++ web/app.py
@@ -1,25 +1,11 @@
 from flask import Flask, render_template, request
-import time
 import sys
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def hello_world(path):
+@app.route('/')
+def hello_world():
     return render_template("index.html")
-
-@app.route('/contact',methods=["POST"])
-def contact():
-    file = open("output/contacts.txt","a")
-    file.write("=====new item=====\n")
-    data = request.get_json()
-    for key in data:
-        file.write(key + '-' + data[key])
-        file.write('\n')
-    file.write('\n')
-    return "success"
-
 
 debug = "debug" in sys.argv
 print("Mode:", debug)
